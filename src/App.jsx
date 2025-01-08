ea9297c9ff8e2239073e7cafb4c62974d268c843
namsh1125 <namsh1125@naver.com>
2025-01-09 00:57:08 +0900

    Feat: 로그인, 회원가입 화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import ReservationPage from './pages/ReservationPage';
+import AuthPage from './pages/AuthPage';
 
 const App = () => {
   const [user, setUser] = useState({ id: 1, name: '남승현', email: 'namsh1125@naver.com' })
 
   return (
     <div>
-      <ReservationPage user={user}/>
+      <AuthPage setUser={setUser} />
     </div>
   );
 };
